806d211a393ee5ea9c5e1d3f6b977e8c96b296f2
ambadi2002 <ambadidashing@gmail.com>
2025-01-16 11:29:57 +0530

    Initial commit

---
+++ Hello World/app.py
@@ -0,0 +1,65 @@
+print("ambadi")
+
+#how to draw a dog
+print("o----")
+print(" ||||")
+
+print('*'* 10)
+
+print('*')
+print('*'*2)
+print('*'*3)
+print('*'*4)
+print('*'*5)
+
+#whatever things that inside the ''/"" are strings(a series of charectors including a,b,c,d.. not numbers)
+# * is used for multiplication
+
+#VARIABLES
+price=10
+price=20
+print(price)
+#never put variables in ''/""
+
+#we can also print float values
+rating=5.9
+print(rating)
+
+#string
+name="ambadi"
+print(name)
+
+#boolean(True/False)
+is_published=True
+print(is_published)
+
+#input fn
+
+#ask a person's name and favourate colour and print "ambadi likes blue
+
+name_new=input('whats your name? ')
+color=input('whats your favourate colour? ')
+print(name_new+' likes '+color)
+
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+
+birth_yr=input("enter your birth year: ")
+age=2024-int(birth_yr)
+print(age)
+
+print(type(age))
+
+#ask a user weight in grams and convert it to kilogran
+
+weight_gm=input("what is your weight in grams? ")
+weight_kg=int(weight_gm)/1000
+print(weight_kg)
+
+#strings
+course="python's course for beginers"
+print(course)
+course='python\'s course for "beginers"'
+print(course)
